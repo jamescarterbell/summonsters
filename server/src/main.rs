e7c82ecbf0d4e4e1e220a5d389cf3d394b36d184
James Bell <jamescarterbell@gmail.com>
2024-10-09 20:01:12 -0400

    First push

---
+++ server/src/main.rs
@@ -0,0 +1,142 @@
+//! Example websocket server.
+//!
+//! Run the server with
+//! ```not_rust
+//! cargo run -p example-websockets --bin example-websockets
+//! ```
+//!
+//! Run a browser client with
+//! ```not_rust
+//! firefox http://localhost:3000
+//! ```
+//!
+//! Alternatively you can run the rust client (showing two
+//! concurrent websocket connections being established) with
+//! ```not_rust
+//! cargo run -p example-websockets --bin example-client
+//! ```
+
+use axum::{
+    body::Body,
+    extract::{
+        ws::{Message, WebSocket, WebSocketUpgrade},
+        Path, State,
+    },
+    http::{Response, StatusCode},
+    response::IntoResponse,
+    routing::{any, get, post},
+    Router,
+};
+use axum_extra::{headers, TypedHeader};
+use dashmap::DashMap;
+
+use std::{borrow::Cow, sync::Arc};
+use std::{collections::HashMap, ops::ControlFlow};
+use std::{net::SocketAddr, path::PathBuf};
+use tower_http::trace::{DefaultMakeSpan, TraceLayer};
+
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+//allows to extract the IP of connecting user
+use axum::extract::connect_info::ConnectInfo;
+use axum::extract::ws::CloseFrame;
+
+//allows to split the websocket stream into separate TX and RX branches
+use futures::{sink::SinkExt, stream::StreamExt};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                format!("{}=debug,tower_http=debug", env!("CARGO_CRATE_NAME")).into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    // build our application with some routes
+    let app = Router::new()
+        .route("/games/:game_id/join", any(ws_handler))
+        .with_state(Arc::new(DashMap::<String, Games>::new()))
+        // logging so we can see whats going on
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(DefaultMakeSpan::default().include_headers(true)),
+        );
+
+    // run it with hyper
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(
+        listener,
+        app.into_make_service_with_connect_info::<SocketAddr>(),
+    )
+    .await
+    .unwrap();
+}
+
+#[axum::debug_handler]
+async fn ws_handler(
+    ws: WebSocketUpgrade,
+    State(games): State<Arc<DashMap<String, Games>>>,
+    user_agent: Option<TypedHeader<headers::UserAgent>>,
+    Path(game_id): Path<String>,
+    ConnectInfo(addr): ConnectInfo<SocketAddr>,
+) -> impl IntoResponse {
+    let user_agent = if let Some(TypedHeader(user_agent)) = user_agent {
+        user_agent.to_string()
+    } else {
+        String::from("Unknown browser")
+    };
+    println!("`{user_agent}` at {addr} connected.");
+
+    if let Some(game) = games.get_mut(&game_id) {
+        ws.on_upgrade(move |socket| game.add_client(socket))
+    } else {
+        Response::builder().status(404).body(Body::empty()).unwrap()
+    }
+}
+
+/// helper to print contents of messages to stdout. Has special treatment for Close.
+fn process_message(msg: Message, who: SocketAddr) -> ControlFlow<(), ()> {
+    match msg {
+        Message::Text(t) => {
+            println!(">>> {who} sent str: {t:?}");
+        }
+        Message::Binary(d) => {
+            println!(">>> {} sent {} bytes: {:?}", who, d.len(), d);
+        }
+        Message::Close(c) => {
+            if let Some(cf) = c {
+                println!(
+                    ">>> {} sent close with code {} and reason `{}`",
+                    who, cf.code, cf.reason
+                );
+            } else {
+                println!(">>> {who} somehow sent close message without CloseFrame");
+            }
+            return ControlFlow::Break(());
+        }
+
+        Message::Pong(v) => {
+            println!(">>> {who} sent pong with {v:?}");
+        }
+        // You should never need to manually handle Message::Ping, as axum's websocket library
+        // will do so for you automagically by replying with Pong and copying the v according to
+        // spec. But if you need the contents of the pings you can see them here.
+        Message::Ping(v) => {
+            println!(">>> {who} sent ping with {v:?}");
+        }
+    }
+    ControlFlow::Continue(())
+}
+
+#[derive(Default)]
+struct Games {}
+
+impl Games {
+    async fn add_client(&mut self, socket: WebSocket) {}
+}
